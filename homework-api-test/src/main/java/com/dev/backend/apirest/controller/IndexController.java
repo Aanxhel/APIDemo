4b12d9c168336117ac9587048548f2c9fa62d6f0
Aanxhel <ang.core92@yahoo.com>
2022-08-03 01:47:23 -0500

    pojo
    creacion de pojo para seteo de datos

--- homework-api-test/src/main/java/com/dev/backend/apirest/controller/IndexController.java
+++ homework-api-test/src/main/java/com/dev/backend/apirest/controller/IndexController.java
@@ -1,16 +1,37 @@
 package com.dev.backend.apirest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dev.backend.apirest.model.Usuario;
 
 //el controlador se encarga de administras las peticiones del usuario
 @Controller
+@RequestMapping("/app")
 public class IndexController {
-	
-	//indica es de tipo GET
-	@GetMapping({"/home","/","/index"})
-	public String index() {
+
+	// indica es de tipo GET
+	@GetMapping({ "/home", "/index" })
+	public String index(ModelMap modelMap) {
+		modelMap.addAttribute("titulo", "Hola spring desde index!");
 		return "index";
 	}
-
+		
+	@GetMapping("/perfil")
+	public String perfil(ModelMap modelMap) {
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre("Juan");
+		usuario.setApellido("Pérez");
+		//usuario.setEmail("corre@mail.com");
+				
+		modelMap.addAttribute("titulo", "Nombre: ".concat(usuario.getNombre()) );
+		modelMap.addAttribute("usuario", usuario);
+		
+		
+		return "perfil";
+	}
+		
 }
