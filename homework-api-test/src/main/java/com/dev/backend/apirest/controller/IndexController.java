a39f84679467d9f192a69ef4285b0946ff9f3aa6
Aanxhel <ang.core92@yahoo.com>
2022-08-08 21:06:50 -0500

    Listar
    se agrega el metodo listar y se crea una List para consumir en una tabla, recuerda que estas son pruebas puntuales y nos centramos en la logica

--- homework-api-test/src/main/java/com/dev/backend/apirest/controller/IndexController.java
+++ homework-api-test/src/main/java/com/dev/backend/apirest/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.dev.backend.apirest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,20 +21,35 @@ public class IndexController {
 		modelMap.addAttribute("titulo", "Hola spring desde index!");
 		return "index";
 	}
-		
+
 	@GetMapping("/perfil")
 	public String perfil(ModelMap modelMap) {
 		Usuario usuario = new Usuario();
-		
+
 		usuario.setNombre("Juan");
 		usuario.setApellido("Pérez");
-		//usuario.setEmail("corre@mail.com");
-				
-		modelMap.addAttribute("titulo", "Nombre: ".concat(usuario.getNombre()) );
+		usuario.setEmail("corre@mail.com");
+
+		modelMap.addAttribute("titulo", "Nombre: ".concat(usuario.getNombre()));
 		modelMap.addAttribute("usuario", usuario);
-		
-		
+
 		return "perfil";
 	}
-		
+
+	@GetMapping("/listar")
+	public String listar(ModelMap modelMap) {
+
+		List<Usuario> usuarios = new ArrayList<>();
+
+		usuarios.add(new Usuario("Angel", "Montiel", "correo@mail.com"));
+		usuarios.add(new Usuario("Juan", "Perez", "correo2@mail.com"));
+		usuarios.add(new Usuario("Pedro", "Gonzalez", "correo3@mail.com"));
+		usuarios.add(new Usuario("Tornado", "Roe", "correo4@mail.com"));
+
+		modelMap.addAttribute("titulo", "Lista de usuarios");
+		modelMap.addAttribute("usuarios", usuarios);
+
+		return "listar";
+	}
+
 }
