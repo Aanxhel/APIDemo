0bc7ed72f9a888740433d02630c9520c19045628
Aanxhel <ang.core92@yahoo.com>
2022-12-09 14:36:16 -0600

    firt commit

--- appwebb/src/main/java/com/shop/notion/appwebb/AppwebbApplication.java
+++ appwebb/src/main/java/com/shop/notion/appwebb/AppwebbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppwebbApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppwebbApplication.class, args);
 	}
