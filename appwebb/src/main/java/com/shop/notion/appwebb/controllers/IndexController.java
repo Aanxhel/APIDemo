5468488c3034833ad4f15d7532090f4ff4763846
Aanxhel <ang.core92@yahoo.com>
2022-12-09 22:06:26 -0600

    create controller

--- appwebb/src/main/java/com/shop/notion/appwebb/controllers/IndexController.java
+++ appwebb/src/main/java/com/shop/notion/appwebb/controllers/IndexController.java
@@ -1,5 +1,14 @@
 package com.shop.notion.appwebb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+	
+	@GetMapping("/app")
+	public String index() {
+		return "index";
+	}
 
 }
