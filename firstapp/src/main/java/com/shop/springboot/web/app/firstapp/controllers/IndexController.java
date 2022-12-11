4a4147437278a60e8474ba18a46ad9d9152e8bda
Aanxhel <ang.core92@yahoo.com>
2022-12-11 12:30:53 -0600

    create new Handler to use profile

--- firstapp/src/main/java/com/shop/springboot/web/app/firstapp/controllers/IndexController.java
+++ firstapp/src/main/java/com/shop/springboot/web/app/firstapp/controllers/IndexController.java
@@ -5,13 +5,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shop.springboot.web.app.firstapp.models.UserModel;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 
+	//muestra el index
 	@GetMapping("/index")
 	public String index(Model model) {
-		model.addAttribute("titulo", "Documento Spring!");
+		model.addAttribute("title", "Document Spring!");
 		return "index";
+	}
+	
+	
+	//crearemos la peticion del POJO
+	@GetMapping("/profile")
+	public String profile(Model model) {
+		
+		UserModel userModel = new UserModel();
+		
+		userModel.setName("Alpha");
+		userModel.setLastName("Power");
+		
+		model.addAttribute("title", "Documento Spring!");
+		model.addAttribute("user", userModel);
+		
+		return "profile";
 	}
 }
