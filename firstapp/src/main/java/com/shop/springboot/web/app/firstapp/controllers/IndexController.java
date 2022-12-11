0574f73072fda0c9f097bbb92f53e3f21dbe79e2
Aanxhel <ang.core92@yahoo.com>
2022-12-10 23:42:56 -0600

    este si jala

--- firstapp/src/main/java/com/shop/springboot/web/app/firstapp/controllers/IndexController.java
+++ firstapp/src/main/java/com/shop/springboot/web/app/firstapp/controllers/IndexController.java
@@ -1,14 +1,13 @@
-package com.shop.notion.appwebb.controllers;
+package com.shop.springboot.web.app.firstapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-	
+
 	@GetMapping("/app")
 	public String index() {
 		return "index";
 	}
-
 }
