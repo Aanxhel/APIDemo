98e3b9adcbeb16bb6e9925ae652800599005aa76
Aanxhel <ang.core92@yahoo.com>
2022-12-11 00:42:04 -0600

    create controller

--- firstapp/src/main/java/com/shop/springboot/web/app/firstapp/controllers/IndexController.java
+++ firstapp/src/main/java/com/shop/springboot/web/app/firstapp/controllers/IndexController.java
@@ -1,13 +1,17 @@
 package com.shop.springboot.web.app.firstapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping("/app")
-	public String index() {
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("titulo", "Documento Spring!");
 		return "index";
 	}
 }
