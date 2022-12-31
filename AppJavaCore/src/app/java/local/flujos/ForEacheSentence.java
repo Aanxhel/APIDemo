f2af8095e8c43b5d44b3ced085522ac2a3905aa7
Aanxhel <ang.core92@yahoo.com>
2022-12-31 00:52:18 -0600

    create class to use for and foreach

---
+++ AppJavaCore/src/app/java/local/flujos/ForEacheSentence.java
@@ -0,0 +1,11 @@
+package app.java.local.flujos;
+
+public class ForEacheSentence {
+	public static void main(String args[]) {
+		 int[] numers = {1,3,5,9,7,11,15};
+		 
+		 for(int number: numers) {
+			 System.out.println("number = " + number);
+		 }
+	}
+}
---
+++ AppJavaCore/src/app/java/local/flujos/SentencesFor.java
@@ -0,0 +1,16 @@
+package app.java.local.flujos;
+
+public class SentencesFor {
+	public static void main(String args[]) {
+
+		String[] nombres = { "Alfa", "Bravo", "Coca" };
+
+		int count = nombres.length;
+
+		for (int i = 0; i < count; i++) {
+			System.out.println(nombres[i]);
+		}
+
+	}
+
+}
