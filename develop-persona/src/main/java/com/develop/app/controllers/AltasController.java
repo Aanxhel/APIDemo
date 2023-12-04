d3e0efa71bafaeb93f2ae13245081d2a9aaee2e1
Aanxhel <ang.core92@yahoo.com>
2023-12-04 01:36:37 -0600

    se agrega el log4j

---
+++ develop-persona/src/main/java/com/develop/app/controllers/AltasController.java
@@ -0,0 +1,22 @@
+package com.develop.app.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class AltasController {
+
+	
+	Logger logger = LogManager.getLogger(AltasController.class);
+	
+	@PostMapping(value = "/holamundo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity helloWorld() {
+		ResponseEntity entity = null;
+
+		logger.info(":::hola mundo:::");
+		entity = ResponseEntity.ok().body("{\"status\":\"ok\"}");
+		return entity;
+	}
+}
---
+++ develop-persona/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>develop app!</h2>
+</body>
+</html>
