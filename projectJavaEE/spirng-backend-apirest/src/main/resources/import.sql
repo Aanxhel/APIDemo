3ca9c8be99682ec6a4f26becc70236aa910221a5
Aanxhel <ang.core92@yahoo.com>
2024-01-10 15:29:25 -0600

    base spring a war

---
+++ projectJavaEE/spirng-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pokemon_tbl (nombre,tipo,generacion,genero) VALUES("bulbasaur","planta",1,"macho");
+INSERT INTO pokemon_tbl (nombre,tipo,generacion,genero) VALUES("squartle","agua",1,"macho");
+INSERT INTO pokemon_tbl (nombre,tipo,generacion,genero) VALUES("blaziken","fuego",3,"hembra");
---
+++ projectJavaEE/spirng-backend-apirest/src/test/java/com/develop/pokemon/app/SpirngBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.pokemon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
