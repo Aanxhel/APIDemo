60d4cdd447f41dda1aa84346861b49a322294a31
Aanxhel <ang.core92@yahoo.com>
2024-01-12 16:04:26 -0600

    peticion local

--- projectSpring/persona-invex-test/src/test/java/com/invex/webapp/personainvextest/PersonaInvexTestApplicationTests.java
+++ projectSpring/persona-invex-test/src/test/java/com/invex/webapp/personainvextest/PersonaInvexTestApplicationTests.java
@@ -1,6 +1,6 @@
 package com.invex.webapp.personainvextest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- projectSpring/spirng-backend-apirest/src/main/java/com/develop/pokemon/app/models/entity/Pokemon.java
+++ projectSpring/spirng-backend-apirest/src/main/java/com/develop/pokemon/app/models/entity/Pokemon.java
@@ -8,13 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name= "pokemon_tbl")
 public class Pokemon implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -24,41 +24,5 @@ public class Pokemon implements Serializable {
 	private String tipo;
 	private Integer generacion;
 	private String genero;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	public Integer getGeneracion() {
-		return generacion;
-	}
-	public void setGeneracion(Integer generacion) {
-		this.generacion = generacion;
-	}
-	public String getGenero() {
-		return genero;
-	}
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
-	
-	
+		
 }
