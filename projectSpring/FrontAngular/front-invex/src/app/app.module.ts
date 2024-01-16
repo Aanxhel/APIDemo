30cc3aed4f59b0234a6f9736b21742a86a4d2f35
Aanxhel <ang.core92@yahoo.com>
2024-01-15 19:05:46 -0600

    se crea con modulo para insercion de datos estaticos

--- projectSpring/FrontAngular/front-invex/src/app/app.module.ts
+++ projectSpring/FrontAngular/front-invex/src/app/app.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EmpleadoComponent } from './empleado/empleado.component';
-import { EmpleadoService } from "./empleado/empleado.service";
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    EmpleadoService,
+   
   ],
   bootstrap: [AppComponent]
 })
