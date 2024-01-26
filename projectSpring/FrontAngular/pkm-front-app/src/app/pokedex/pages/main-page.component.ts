7362afbdd145f6062d26666bac7b743ec801ab19
Aanxhel <ang.core92@yahoo.com>
2024-01-26 16:59:44 -0600

    acutalizaciuon de front y eliminacion de proywecto fallido de spring

---
+++ projectSpring/FrontAngular/pkm-front-app/src/app/pokedex/pages/main-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+}
